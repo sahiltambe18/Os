17a0edd14f297d68b16161c191f32935545c3cd6
sahil <75826199+sahiltambe18@users.noreply.github.com>
2023-11-19 21:31:41 +0530

    Update search

--- addrBook.sh
+++ addrBook.sh
@@ -1,3 +1,12 @@
+# 
+# Welcome to GDB Online.
+# GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+# C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+# Code, Compile, Run and Debug online from anywhere in world.
+# 
+# 
+echo "Hello World";
+
 
 touch emprecord.txt
 
@@ -7,7 +16,8 @@ echo "1 View all record"
 echo "2 Insert a record"
 echo "3 delete a record"
 echo "4 Modify a record"
-echo "5 Exit"
+echo "5 search a record"
+echo "6 Exit"
 
 echo "Enter your choice"
 read choice
@@ -38,9 +48,14 @@ case $choice in
     ;;
    
    5)
+    echo -e "Enter search keyword"
+    read keyword
+    grep -i "$keyword" emprecord.txt
+    ;;
+   
+  6)
     echo "Exited"
     exit
     ;;
 esac
 done
-
